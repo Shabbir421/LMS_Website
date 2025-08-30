32fef064ad9abead8cb37e492476312f3f106f3b
Shabbir <shabbiransari421@gmail.com>
2025-08-30 16:33:51 +0530

    ddd

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -1,13 +1,17 @@
 /** @format */
 
 import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
 
-const connectCloudinary = async () => {
+dotenv.config(); // make sure .env loads
+
+const connectCloudinary = () => {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET, // ✅ fixed key name
+    api_secret: process.env.CLOUDINARY_API_SECRET, // ✅ correct key
   });
+  console.log("✅ Cloudinary connected");
 };
 
 export default connectCloudinary;
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ import connectDB from "./configs/mongodb.js";
 import { clerkWebhooks, stripeWebhooks } from "./controllers/webhooks.js";
 import educatorRouter from "./routes/educatorRoutes.js";
 import { clerkMiddleware } from "@clerk/express";
-import connectCloudinay from "./configs/cloudinary.js";
+import connectCloudinary from "./configs/cloudinary.js";
 import courseRouter from "./routes/courseRoute.js";
 import userRouter from "./routes/userRoutes.js";
 
@@ -16,7 +16,7 @@ const app = express();
 
 // connect to db
 await connectDB();
-await connectCloudinay();
+await connectCloudinary();
 
 // middleware
 app.use(cors());
