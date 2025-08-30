3d5bdf7d3893409cc549319ed1ea5d7c387977f2
Shabbir <shabbiransari421@gmail.com>
2025-08-30 16:19:13 +0530

    dd

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -1,11 +1,13 @@
-import {v2 as cloudinary} from 'cloudinary'
+/** @format */
 
-const connectCloudinay = async()=>{
-    cloudinary.config({
-        cloud_name:process.env.CLOUDINARY_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_SECRET_KEY
-    })
-}
+import { v2 as cloudinary } from "cloudinary";
 
-export default connectCloudinay;
+const connectCloudinary = async () => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET, // ✅ fixed key name
+  });
+};
+
+export default connectCloudinary;
